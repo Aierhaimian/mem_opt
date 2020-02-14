cc737a7e934ebc63b62ed391669e83567506427d
Earl du <duzexu92@gmail.com>
2020-02-14 15:21:41 +0800

    [Edit] ugly test go go go!
    Signed-off-by: Earl du <duzexu92@gmail.com>

--- main.c
+++ main.c
@@ -1,10 +1,47 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/time.h>
 
 #include "my_test.h"
 
+#define NUM 100000
+#define VAL 65535
 
-
-void main()
+int main()
 {
+    struct timeval tv1, tv2;
+    long test_time;
+    pid_t pid;
+    int arr_1[NUM];
+    uint16_t arr_2[NUM];
+    uint32_t arr_3[NUM];
 
+    for (int i=0; i<NUM; i++)
+    {
+        arr_1[i] = i;
+        arr_2[i] = i;
+        arr_3[i] = i;
+    }
+
+    pid = fork();
+    
+    if (pid < 0) {
+        printf("fork error!\n");
+        exit(1);
+    }else if (pid == 0) {
+        gettimeofday(&tv1, NULL);
+        memset(arr_1, VAL, NUM);
+        gettimeofday(&tv2, NULL);
+
+        test_time = (tv2.tv_sec - tv1.tv_sec)*1000000 + (tv2.tv_usec - tv1.tv_usec);
+
+        printf("the time of memset 100000 numbers in memory is: %ld us.\n", test_time);
+    }else {
+        printf("I am a father process.\n");
+    }
+
+
+    return 0;
 }
